0fdd51e74fde7876a7d8ab868b2be0c3a0e55f89
user <4689asgf@naver.com>
2021-08-19 02:17:55 +0900

    21.8.9 | AM 2:17 (Seo)
    read

---
+++ lib/Cartoon/foodwaste.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class foodwaste extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('App info'),
+    );
+  }
+}
